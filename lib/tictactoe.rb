3aee91e63cf477ac134c85beeb29dc26fd168044
Gabriela <gabriela.guedes.gmg@gmail.com>
2023-03-13 22:42:10 -0300

    add computer player

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,15 +1,15 @@
 require_relative './tictactoe/board.rb'
 require_relative './tictactoe/human_player.rb'
+require_relative './tictactoe/computer_player.rb'
 
 class Game
 
-  attr_accessor :board, :player_one
+  attr_accessor :board, :player_one, :player_two
 
   def initialize
     @board = Tictactoe::Board.new
     @player_one = Tictactoe::HumanPlayer.new(Tictactoe::Board::MARKERS[1], board)
-    @com = 'X' # the computer's marker
-    @hum = 'O' # the user's marker
+    @player_two = Tictactoe::ComputerPlayer.new(Tictactoe::Board::MARKERS[0], board)
   end
 
   def start_game
@@ -19,56 +19,10 @@ class Game
     # loop through until the game was won or tied
     until board.game_is_over_with_winner? || board.tie?
       player_one.play_turn
-      eval_board if !board.game_is_over_with_winner? && !board.tie?
+      player_two.play_turn if !board.game_is_over_with_winner? && !board.tie?
       board.print_board
     end
     puts 'Game over'
-  end
-
-  def eval_board
-    spot = nil
-    until spot
-      if board.center_spot_available?
-        spot = 4
-        board.fill_spot(spot, @com)
-      else
-        spot = get_best_move
-        if board.empty_spot?(spot)
-          board.fill_spot(spot, @com)
-        else
-          spot = nil
-        end
-      end
-    end
-  end
-
-  def get_best_move
-    available_spaces = []
-    best_move = nil
-    board.to_a.each do |s|
-      available_spaces << s if s != 'X' && s != 'O'
-    end
-    available_spaces.each do |as|
-      board.fill_spot(as.to_i, @com)
-      if board.game_is_over_with_winner?
-        best_move = as.to_i
-        board.fill_spot(as.to_i, as)
-        return best_move
-      else
-        board.fill_spot(as.to_i, @hum)
-        if board.game_is_over_with_winner?
-          best_move = as.to_i
-          board.fill_spot(as.to_i, as)
-          return best_move
-        else
-          board.fill_spot(as.to_i, as)
-        end
-      end
-    end
-    return best_move if best_move
-
-    n = rand(0..available_spaces.count)
-    available_spaces[n].to_i
   end
 end
 
---
+++ lib/tictactoe/computer_player.rb
@@ -0,0 +1,51 @@
+require_relative './player.rb'
+require_relative './board.rb'
+
+module Tictactoe
+  class ComputerPlayer < Player
+
+    def play_turn
+      fill_spot(spot)
+    end
+
+    private
+
+    def spot
+      return center_spot if board.center_spot_available?
+
+      get_best_move
+    end
+
+    def get_best_move
+      board.available_spots.each do |available_spot|
+        return available_spot if self_wins_with_spot?(available_spot) || enemy_wins_with_spot?(available_spot)
+
+        reset_board(available_spot)
+      end
+
+      board.random_available_spot      
+    end
+
+    def self_wins_with_spot?(available_spot)
+      fill_spot(available_spot)
+      return board.game_is_over_with_winner?
+    end
+
+    def enemy_wins_with_spot?(available_spot)
+      board.fill_spot(available_spot, other_player_marker)
+      return board.game_is_over_with_winner?
+    end
+
+    def reset_board(available_spot)
+      board.fill_spot(available_spot, available_spot.to_s)
+    end
+
+    def other_player_marker
+      (Tictactoe::Board::MARKERS - [marker]).first
+    end
+
+    def center_spot
+      Tictactoe::Board::CENTER_SPOT_INDEX
+    end
+  end
+end
