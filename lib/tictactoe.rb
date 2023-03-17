d0db8204028642aee7b664a54263409e8eb23c5e
Gabriela <gabriela.guedes.gmg@gmail.com>
2023-03-16 21:54:49 -0300

    improve game class

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -3,8 +3,11 @@ require_relative './tictactoe/human_player.rb'
 require_relative './tictactoe/computer_player.rb'
 
 class Game
-
   attr_accessor :board, :player_one, :player_two
+
+  def self.start_game
+    new.start_game
+  end
 
   def initialize
     @board = Tictactoe::Board.new
@@ -13,17 +16,25 @@ class Game
   end
 
   def start_game
-    # start by printing the board
-    board.print_board
-    puts 'Enter [0-8]:'
-    # loop through until the game was won or tied
-    until board.game_is_over_with_winner? || board.tie?
-      player_one.play_turn
-      player_two.play_turn if !board.game_is_over_with_winner? && !board.tie?
-      board.print_board
-    end
+    print_game_start
+    run_game while !game_finished?
     puts 'Game over'
   end
-end
 
-Game.new.start_game
+  private
+
+  def print_game_start
+    board.print_board
+    puts 'Enter [0-8]:'
+  end
+
+  def run_game
+    player_one.play_turn
+    player_two.play_turn if !game_finished?
+    board.print_board
+  end
+
+  def game_finished?
+    board.game_is_over_with_winner? || board.tie?
+  end
+end
---
+++ spec/lib/tictactoe_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../../lib/tictactoe.rb'
+require_relative '../../lib/tictactoe/computer_player.rb'
+require_relative '../../lib/tictactoe/human_player.rb'
+require_relative '../../lib/tictactoe/board.rb'
+
+RSpec.describe Game do
+  describe '#start_game' do
+    let(:game) { described_class.new }
+    let(:specific_inputs) { %w[0 1 3] }
+
+    subject(:empty_spot) { board.empty_spot?(index) }
+
+    it 'finishes the game after 3 rounds (with specific inputs)' do
+      allow_any_instance_of(Tictactoe::HumanPlayer).to receive(:gets).and_return(*specific_inputs)
+      game.start_game
+      expect(game.board.game_is_over_with_winner? || game.board.tie?).to eq(true)            
+    end
+  end
+
+  describe '#initialize' do
+    subject(:intialize_game) { described_class.new }
+
+    it 'intanciates the board and two players' do
+      expect(Tictactoe::Board).to receive(:new)
+      expect(Tictactoe::HumanPlayer).to receive(:new)
+      expect(Tictactoe::ComputerPlayer).to receive(:new)
+      intialize_game
+    end
+  end
+end
