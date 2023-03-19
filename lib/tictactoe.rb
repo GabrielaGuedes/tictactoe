45a281d232df89cf9dca9389d884f51abb79b19f
Gabriela <gabriela.guedes.gmg@gmail.com>
2023-03-19 16:34:17 -0300

    fix requires

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,3 +1,6 @@
 require_relative './tictactoe/game.rb'
+require_relative './tictactoe/human_vs_human_game.rb'
+require_relative './tictactoe/human_vs_computer_game.rb'
+require_relative './tictactoe/computer_vs_computer_game.rb'
 
-Game.start
+Tictactoe::Game.start
