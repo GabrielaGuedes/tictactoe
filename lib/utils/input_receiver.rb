f39c9bb90cbf5f42ab41b6e80af94974693a1e3c
Gabriela <gabriela.guedes.gmg@gmail.com>
2023-03-20 20:15:08 -0300

    add input receiver class

---
+++ lib/utils/input_receiver.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Utils
+  class InputReceiver
+
+    def self.valid_input(message, &validation_block)
+      puts message
+      input = gets.chomp
+      return input if valid?(input) { |i| validation_block.call(i) }
+
+      puts 'Invalid input, try again.'
+      valid_input(message) { |i| validation_block.call(i) }
+    end
+
+    def self.valid?(input, &validation_block)
+      validation_block.call(input)
+    end
+  end
+end
---
+++ spec/lib/utils/input_receiver_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../../../lib/utils/input_receiver'
+
+RSpec.describe Utils::InputReceiver do
+  let(:desired_input) { 'a' }
+  let(:message) { 'Enter your input' }
+  subject(:valid_input) { described_class.valid_input(message) { |i| i == desired_input } }
+
+  describe '#self.valid_input' do
+    context 'when provided input is valid' do
+      it 'returns the input' do
+        allow(described_class).to receive(:gets).and_return(desired_input)
+        expect(valid_input).to eq(desired_input)
+      end
+    end
+
+    context 'when input is not valid' do
+      let(:wrong_input) { 'b' }
+
+      it 'asks another input' do
+        allow(described_class).to receive(:gets).and_return(wrong_input, desired_input)
+        expect(valid_input).to eq(desired_input)        
+      end
+    end
+  end
+end
